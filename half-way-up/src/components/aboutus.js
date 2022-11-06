fc767de61c97c21af7421423fce6f71fdfd6ab8f
Ryan <rcgrant012@gmail.com>
2022-11-05 22:02:02 -0400

    edited about us

--- half-way-up/src/components/aboutus.js
+++ half-way-up/src/components/aboutus.js
@@ -2,6 +2,8 @@ import { Container, Row, Col } from "react-bootstrap";
 import 'react-multi-carousel/lib/styles.css';
 import TrackVisibility from 'react-on-screen';
 import colorSharp from "../assets/img/color-sharp.png"
+import contactImg from "../assets/img/logo.png";
+import logo from '../assets/img/logo.svg';
 
 export const AboutUs = () => {
 
@@ -12,12 +14,13 @@ export const AboutUs = () => {
         <Row className="align-items-center">
           <Col size={12} md={12}>
             <h2>
-              About Us
+              <br></br>About Us
             </h2>
             <h4>
-            We are Half Way Down, and our goal is to bring together all the up and coming startup companies across 
+            We are the Half Way Down team, and our goal is to bring together all the up and coming startup companies across 
             industries into one, easy and accessible website!
             </h4>
+          </Col>
             <Col size={12} md={6}>
               <p>
               Our team has noticed that startup companies often take the backseat when it comes to recruiting, trading, and networking. 
@@ -26,7 +29,9 @@ export const AboutUs = () => {
               advertise their brand to students and interested hobbyists, and engage in business transactions directly, circumventing the middleman.
               </p>
             </Col>
-          </Col>
+            <Col size={12} md={3}>
+              <img src="logo.png" alt="Contact Us" width="200" height="200" />
+            </Col>
         </Row>
       </Container>
         <img className="background-image-left" src={colorSharp} alt="Image" />
