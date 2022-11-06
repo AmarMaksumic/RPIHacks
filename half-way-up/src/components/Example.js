aa4953c444fd625f5a14c418432680a36b89a673
Ryan <rcgrant012@gmail.com>
2022-11-06 00:48:00 -0400

    doing example

--- half-way-up/src/components/Example.js
+++ half-way-up/src/components/Example.js
@@ -1,25 +1,26 @@
 import { Container, Row, Col } from "react-bootstrap";
 import colorSharp from "../assets/img/color-sharp.png";
+import logo from '../assets/img/GF-logo.png';
 
 export const Example = () => {
     return (    
     <section className="aboutus" id="aboutus">
         <Container>
-            <Row md={1}>
-                <Col md={11}>
-                <Row md={1}/>
-                <Row md={8}>
-                    <Col md={1}/>
-                    <Col md={5}>
-                        <div>
-                            Heelo
-                        </div>
-                    </Col>
-                    <Col md={2}/>
-                </Row>
-                <Row md={2}/>
-                </Col>
-            </Row> 
+            <Row md={8}>
+              <Col size={6} md={1}><img src={logo} alt="Your Logo Here" className="company-logo"/></Col>
+              
+              <Col size={6} md={11} style={{paddingTop: "auto", paddingBottom: "auto"}}> 
+              
+              <h3 style={{margin: "auto auto"}}>Global Foundries</h3>
+              </Col>
+            </Row>
+            <Row md={4}>
+              <Col size={6} md={1}></Col>
+              <Col size={6} md={11}> @GlobalFoundries123 </Col>
+            </Row>
+            <Row md={6}>
+              great
+            </Row>
         </Container>
         <img className="background-image-left" src={colorSharp} alt="Image" />
     </section>
